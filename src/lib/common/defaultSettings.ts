c648d67b809780647c02407f28cacace1583e63b
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2023-11-07 12:35:37 +0800

    refactor(Settings): add default settings for guild and user

---
+++ src/lib/common/defaultSettings.ts
@@ -0,0 +1,32 @@
+export const defaultGuildSettings: GuildSettings = {
+    tags: [],
+    badges: []
+}
+
+export const defaultUserSettings: UserSettings = {
+    userBadges: []
+}
+
+export interface UserSettings {
+    userBadges: Badge[];
+}
+
+export interface GuildSettings {
+    tags: Tag[];
+    badges: Badge[];
+}
+
+interface Tag {
+    name: string;
+    content: string;
+    author: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+interface Badge {
+    name: string;
+    description: string;
+    createdAt: string;
+    createdBy: string;
+}
