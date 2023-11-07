921886f6874a01f1d65cd6f23e3e646ae3f7c29b
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2023-11-07 13:36:19 +0800

    feat: add user profile command

---
+++ src/commands/General/profile.ts
@@ -0,0 +1,46 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+import { EmbedBuilder, time } from 'discord.js';
+
+@ApplyOptions<Command.Options>({
+	description: "View a user's profile.",
+	requiredClientPermissions: ['EmbedLinks']
+})
+export class UserCommand extends Command {
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder
+				.setName(this.name)
+				.setDescription(this.description)
+				.addUserOption((option) => option.setName('user').setDescription('The user to view the profile of.').setRequired(false))
+		);
+	}
+
+	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		const user = interaction.options.getUser('user') ?? interaction.user;
+		const member = await interaction.guild?.members.fetch(user.id);
+
+		const userSettings = await this.container.settings.getUserSetting(user.id);
+		const badges = userSettings.userBadges.sort((a, b) => b.createdAt - a.createdAt);
+
+		const embed = new EmbedBuilder()
+			.setColor('#739072')
+			.setTitle(`${user.username}'s Profile`)
+			.setThumbnail(user.displayAvatarURL({ size: 2048, extension: 'webp' }))
+			.addFields([
+				{ name: 'Created at', value: `${time(user.createdAt, 'f')} (${time(user.createdAt, 'R')})`, inline: true },
+				{
+					name: 'Joined at',
+					value: member?.joinedAt ? `${time(member?.joinedAt!, 'f')} (${time(member.joinedAt, 'R')})` : 'Unknown',
+					inline: true
+				},
+				{ name: 'Badges', value: badges.length ? badges.map((badge) => badge.name).join(', ') : 'This user has no badges.' }
+			])
+			.setFooter({
+				text: `Requested by ${interaction.user.username}`,
+				iconURL: interaction.user.displayAvatarURL({ size: 128, extension: 'webp' })
+			});
+
+		return interaction.reply({ embeds: [embed] });
+	}
+}
