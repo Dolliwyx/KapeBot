b398b526429e769f41d0c5a677f7c1ca639b2f87
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2023-11-23 22:08:50 +0800

    refactor: update database settings in setup

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -9,8 +9,10 @@ import '@sapphire/plugin-subcommands/register';
 import { setup } from '@skyra/env-utilities';
 import * as colorette from 'colorette';
 import { inspect } from 'util';
-import { rootDir, srcDir } from '#lib/constants';
-import { Settings } from '#lib/structures/Settings';
+import { srcDir } from '#lib/constants';
+import { Setting } from './structures/Setting';
+import { User } from './schemas/User';
+import { Guild } from './schemas';
 
 // Set default behavior to bulk overwrite
 ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.BulkOverwrite);
@@ -24,4 +26,8 @@ inspect.defaultOptions.depth = 1;
 // Enable colorette
 colorette.createColors({ useColor: true });
 
-container.settings = new Settings(new URL(process.env.DATABASE_URL, rootDir));
+const dbUrl = `postgres://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`
+
+container.logger.info('Readying database...');
+container.settings.users = new Setting('userSettings', 'postgres', new URL(dbUrl), User);
+container.settings.guilds = new Setting('guildSettings', 'postgres', new URL(dbUrl), Guild);
