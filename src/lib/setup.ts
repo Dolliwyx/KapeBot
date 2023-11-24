e7b57734e818f9934f5937058c96a3e5b041fb00
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2023-11-24 18:25:00 +0800

    refactor: move setting initialization on client

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -1,7 +1,7 @@
 // Unless explicitly defined, set NODE_ENV as development:
 process.env.NODE_ENV ??= 'development';
 
-import { ApplicationCommandRegistries, RegisterBehavior, container } from '@sapphire/framework';
+import { ApplicationCommandRegistries, RegisterBehavior } from '@sapphire/framework';
 import '@sapphire/plugin-api/register';
 import '@sapphire/plugin-editable-commands/register';
 import '@sapphire/plugin-logger/register';
@@ -10,9 +10,6 @@ import { setup } from '@skyra/env-utilities';
 import * as colorette from 'colorette';
 import { inspect } from 'util';
 import { srcDir } from '#lib/constants';
-import { Setting } from './structures/Setting';
-import { User } from './schemas/User';
-import { Guild } from './schemas';
 
 // Set default behavior to bulk overwrite
 ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.BulkOverwrite);
@@ -25,9 +22,3 @@ inspect.defaultOptions.depth = 1;
 
 // Enable colorette
 colorette.createColors({ useColor: true });
-
-const dbUrl = `postgres://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
-
-container.logger.info('Readying database...');
-container.settings.users = new Setting('userSettings', 'postgres', new URL(dbUrl), User);
-container.settings.guilds = new Setting('guildSettings', 'postgres', new URL(dbUrl), Guild);
--- src/lib/structures/KapeClient.ts
+++ src/lib/structures/KapeClient.ts
@@ -1,5 +1,7 @@
 import { LogLevel, SapphireClient, container } from '@sapphire/framework';
 import { GatewayIntentBits, Partials } from 'discord.js';
+import { Setting } from '#lib/structures/Setting';
+import { Guild, User } from '#schemas/index';
 
 export class KapeClient extends SapphireClient {
 	constructor() {
@@ -33,7 +35,12 @@ export class KapeClient extends SapphireClient {
 	}
 
 	public override async login(token?: string) {
-		this.logger.info('Readying database...');
+		this.logger.info('[Settings] Readying database...');
+		const dbUrl = `postgres://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
+		container.settings = {
+			users: new Setting('userSettings', 'postgres', new URL(dbUrl), User),
+			guilds: new Setting('guildSettings', 'postgres', new URL(dbUrl), Guild)
+		};
 
 		return super.login(token);
 	}
